98059e555b2cddfac1d2b2675108293fe0c1d42c
Carlos <cbc_sg@yahoo.es>
2020-11-04 08:27:54 +0000

    add readme.md notes

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 This project is a example of java swing boot project
+
+
+use the next environment variables:
+- spring.datasource.url = ${DB_URL}
+- spring.datasource.username = ${DB_USER}
+- spring.datasource.password =${DB_PASSWORD}
+
+use the next url to access swager developer site:
+- http://localhost:5000/swagger-ui.html
--- src/main/java/es/test/springboot/config/SwaggerConfig.java
+++ src/main/java/es/test/springboot/config/SwaggerConfig.java
@@ -8,6 +8,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
@@ -17,7 +18,7 @@ public class SwaggerConfig {
                 .select()
                 .apis(
                         RequestHandlerSelectors
-                        .basePackage("es.test.demo.controllers")
+                        .basePackage("es.test.springboot.controllers")
                 )
                 .paths(PathSelectors.any())
                 .build();
